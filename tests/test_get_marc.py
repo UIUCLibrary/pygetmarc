6fd562fcdbe99af0e543083517a6dd5107b96b8c
hborcher <hborcher@illinois.edu>
2018-02-12 12:24:22 -0600

    whitespace

--- tests/test_get_marc.py
+++ tests/test_get_marc.py
@@ -2,12 +2,14 @@ import os
 
 from uiucprescon import pygetmarc
 
+
 def test_get_marc():
     with open(os.path.join(os.path.dirname(__file__), "1099891.marc"), "rU", encoding="utf-8-sig") as marc_file:
         expected_marc_data = marc_file.read()
 
     marc_record = pygetmarc.request.get_marc(bib_id="1099891")
     assert expected_marc_data == marc_record
+
 
 def test_get_marc_validated():
     with open(os.path.join(os.path.dirname(__file__), "1099891.marc"), "rU", encoding="utf-8-sig") as marc_file:
@@ -21,7 +23,6 @@ def test_get_marc_validated():
             new_expected_marc_data_lines.append(line)
 
         expected_marc_data = "\n".join(new_expected_marc_data_lines)
-
 
     marc_record = pygetmarc.request.get_marc(bib_id="1099891", validate=True)
     assert expected_marc_data == marc_record
