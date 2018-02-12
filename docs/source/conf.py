208887bcb34860767a95d015fb9983532ab677fd
hborcher <hborcher@illinois.edu>
2018-02-12 11:07:37 -0600

    added logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,6 +97,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/full_mark_horz_bw.gif'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -112,6 +113,8 @@ html_sidebars = {
         'donate.html',
     ]
 }
+
+
 
 
 # -- Options for HTMLHelp output ------------------------------------------
