5f8cad732e84fb2743f0e212b9d9c9ff2fee31e2
hborcher <hborcher@illinois.edu>
2018-02-12 12:24:49 -0600

    added params and clean up data

--- uiucprescon/pygetmarc/request.py
+++ uiucprescon/pygetmarc/request.py
@@ -2,19 +2,28 @@ import aiohttp
 import asyncio
 import async_timeout
 
-async def fetch(session, url):
+
+async def fetch(session, url, params):
     with async_timeout.timeout(10):
-        async with session.get(url) as response:
-            return await response.text()
+        async with session.get(url, params=params) as response:
+            return await response.text(encoding="utf-8-sig")
 
 
-async def get_marc_async():
+def clean_up(data: str):
+    return data.replace("\r\n", "\n")
+
+
+async def get_marc_async(bib_id, validate=False):
+    params = {}
+    url = f"http://quest.library.illinois.edu/GetMARC/one.aspx/{bib_id}.marc"
+    if validate:
+        params['v'] = "true"
     async with aiohttp.ClientSession() as session:
-        http = await fetch(session=session, url="http://quest.library.illinois.edu/GetMARC/one.aspx/1099891.marc")
-        return http
+        http = await fetch(session=session, url=url, params=params)
+        return clean_up(data=http)
 
 
-def get_marc(bib_id):
+def get_marc(bib_id, validate=False):
     loop = asyncio.get_event_loop()
-    value = loop.run_until_complete(get_marc_async())
+    value = loop.run_until_complete(get_marc_async(bib_id=bib_id, validate=validate))
     return value
