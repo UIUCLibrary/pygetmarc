2c5f2b9906752ba929cd01f772d27d08675a8801
Henry Borchers <henryborchers@yahoo.com>
2018-02-12 22:18:30 -0600

    parametrize tests

--- tests/test_get_marc.py
+++ tests/test_get_marc.py
@@ -1,28 +1,20 @@
 import os
-
+import pytest
 from uiucprescon import pygetmarc
 
 
-def test_get_marc():
-    with open(os.path.join(os.path.dirname(__file__), "1099891.marc"), "rU", encoding="utf-8-sig") as marc_file:
+def get_sample_record(bib_id):
+    with open(os.path.join(os.path.dirname(__file__), f"{bib_id}.marc"), "rU", encoding="utf-8-sig") as marc_file:
         expected_marc_data = marc_file.read()
+    return expected_marc_data.strip()
 
-    marc_record = pygetmarc.request.get_marc(bib_id="1099891")
+
+@pytest.mark.parametrize("bib_id,validate", [
+    (1099891, False),
+    (1099891, True),
+])
+def test_get_marc(bib_id, validate):
+    expected_marc_data = get_sample_record(bib_id)
+    marc_record = pygetmarc.request.get_marc(bib_id, validate)
     assert expected_marc_data == marc_record
 
-
-def test_get_marc_validated():
-    with open(os.path.join(os.path.dirname(__file__), "1099891.marc"), "rU", encoding="utf-8-sig") as marc_file:
-        expected_marc_data = marc_file.read()
-        expected_marc_data_lines = expected_marc_data.split("\n")
-        new_expected_marc_data_lines = []
-        for line in expected_marc_data_lines:
-            if '<?count 1 ?>' in line:
-                new_expected_marc_data_lines.append('<?v true ?>')
-
-            new_expected_marc_data_lines.append(line)
-
-        expected_marc_data = "\n".join(new_expected_marc_data_lines)
-
-    marc_record = pygetmarc.request.get_marc(bib_id="1099891", validate=True)
-    assert expected_marc_data == marc_record
