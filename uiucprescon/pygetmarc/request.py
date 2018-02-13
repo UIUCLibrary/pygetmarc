58efb143b0cf78e1fbee560f442a26feb93c0720
Henry Borchers <henryborchers@yahoo.com>
2018-02-12 23:06:31 -0600

    added type hints

--- uiucprescon/pygetmarc/request.py
+++ uiucprescon/pygetmarc/request.py
@@ -1,9 +1,11 @@
+import typing
+
 import aiohttp
 import asyncio
 import async_timeout
 
 
-async def fetch(session, url, params) -> str:
+async def fetch(session: aiohttp.ClientSession, url: str, params: typing.Dict[str, str]) -> typing.Any:
     with async_timeout.timeout(10):
         async with session.get(url, params=params) as response:
             return await response.text(encoding="utf-8-sig")
@@ -21,7 +23,7 @@ def clean_up(data: str) -> str:
     return "\n".join(lines)
 
 
-async def get_marc_async(bib_id, validate=False):
+async def get_marc_async(bib_id: typing.Union[int, str], validate: bool = False) -> str:
     params = {}
     url = f"http://quest.library.illinois.edu/GetMARC/one.aspx/{bib_id}.marc"
 
@@ -34,7 +36,18 @@ async def get_marc_async(bib_id, validate=False):
         return cleaned_up_data
 
 
-def get_marc(bib_id, validate=False):
+def get_marc(bib_id: typing.Union[int, str], validate: bool = False) -> str:
+    """
+    Get the marc data from a giving bib_id
+
+    Args:
+        bib_id: The bib id from a Voyager record
+        validate: MARC XML returned from the Z39.50 target should be validated against the XML schema before further
+        processing.
+
+    Returns: Marc XML data
+
+    """
     loop = asyncio.get_event_loop()
     value = loop.run_until_complete(get_marc_async(bib_id=bib_id, validate=validate))
     return value
