4d7a667f1507d769df5cf0699506f25e0aee0b06
Henry Borchers <henryborchers@yahoo.com>
2018-02-12 22:17:38 -0600

    clean up marc data

--- uiucprescon/pygetmarc/request.py
+++ uiucprescon/pygetmarc/request.py
@@ -3,24 +3,35 @@ import asyncio
 import async_timeout
 
 
-async def fetch(session, url, params):
+async def fetch(session, url, params) -> str:
     with async_timeout.timeout(10):
         async with session.get(url, params=params) as response:
             return await response.text(encoding="utf-8-sig")
 
 
-def clean_up(data: str):
-    return data.replace("\r\n", "\n")
+def clean_up(data: str) -> str:
+    # fix each line so that it has the proper line endings
+    lines = []
+    for i, line in enumerate(data.splitlines()):
+        # remove any Processing Instruction
+        if "<?" in line:
+            continue
+        lines.append(line)
+
+    return "\n".join(lines)
 
 
 async def get_marc_async(bib_id, validate=False):
     params = {}
     url = f"http://quest.library.illinois.edu/GetMARC/one.aspx/{bib_id}.marc"
+
     if validate:
         params['v'] = "true"
+
     async with aiohttp.ClientSession() as session:
         http = await fetch(session=session, url=url, params=params)
-        return clean_up(data=http)
+        cleaned_up_data = clean_up(data=http)
+        return cleaned_up_data
 
 
 def get_marc(bib_id, validate=False):
